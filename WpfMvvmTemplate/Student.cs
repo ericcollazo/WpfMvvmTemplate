6788dd4fcad5d439296a6894a823d27b512efe1a
Eric Collazo <ecollazo@dhs.nyc.gov>
2016-07-14 11:02:15 -0400

    Initial template state.

---
+++ WpfMvvmTemplate/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WpfMvvmTemplate
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime JoiningDate { get; set; }
+    }
+}
